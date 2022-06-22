66b8e83b978e4fd884a2a770ad74227ec1e002ec
Jacob Templeton <jwctempleton@gmail.com>
2022-06-21 21:19:20 -0700

    Add change to state to make an array of objects to hold value and whether the particular number in state is being held (bool)

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -8,7 +8,12 @@ function App() {
     for (let i = 0; i < 10; i++) {
       numArray.push(Math.floor(Math.random() * 6) + 1);
     }
-    return numArray;
+
+    const numObject = numArray.map((num) => ({
+      value: num,
+      isHeld: false,
+    }));
+    return numObject;
   }
 
   function handleRoll() {
@@ -17,7 +22,7 @@ function App() {
 
   const [dice, setDice] = React.useState(allNewDice());
 
-  const diceElement = dice.map((die) => <Die value={die} />);
+  const diceElement = dice.map((die) => <Die value={die.value} />);
   return (
     <main>
       <div className="dice-container">{diceElement}</div>
